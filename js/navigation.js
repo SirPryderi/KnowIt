3fdfde7b86f58628eb592cd3deab0589964290bb
Vittorio Iocolano <pryderi.mail@gmail.com>
2017-11-26 19:36:57 +0000

    Added placeholder alerts

--- js/navigation.js
+++ js/navigation.js
@@ -5,7 +5,7 @@ $('a:not(.collapsed)').click(function (e) {
     if (location.indexOf('#') === 0 && location.length > 1) {
         openSection(location);
 
-        $('.nav-item[data-page="'+location.substr(1)+'"]').addClass('active').siblings().removeClass('active');
+        $('.nav-item[data-page="' + location.substr(1) + '"]').addClass('active').siblings().removeClass('active');
 
         e.preventDefault();
     }
@@ -21,4 +21,20 @@ var challenge = undefined;
 
 $('.start-challenge').click(function () {
     challenge = new ChallengeController();
+});
+
+
+$('a:contains("share")').click(function (e) {
+   alert('Share function not present in the prototype.');
+   e.preventDefault();
+});
+
+$('a:contains("favorite")').click(function (e) {
+    alert('Like function not present in the prototype.');
+    e.preventDefault();
+});
+
+$('a:contains("comment")').click(function (e) {
+    alert('Comment function not present in the prototype.');
+    e.preventDefault();
 });
